7064d92ddde4788ba6c0350ec84d9f15206684f1
Thomas Nemec <30568242+thncode@users.noreply.github.com>
2019-01-27 18:52:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var hostname = os.hostname();
 const fs = require('fs');
 const moment = require('moment');
 
-const readFile = "/home/pi/WeatherStation/data.txt";
+const readFile = "/root/.homebridge/weatherstation.txt";
 
 var rain, battery, alertLevel, readTime, raining, wasRaining;
 var lastActivation, lastReset, lastChange, timesOpened, timeOpen, timeClose;
